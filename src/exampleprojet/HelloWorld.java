8d8675d73666b6da38e108adeb683cb1e5efadb9
Your Name <you@example.com>
2019-11-06 16:58:03 +0100

    frist commit

---
+++ src/exampleprojet/HelloWorld.java
@@ -0,0 +1,20 @@
+package exampleprojet;
+
+public class HelloWorld {
+	
+	
+	public String getMessage(String name)
+	{
+		
+		if(name!=null)
+			return " bonjour "+name;
+		else
+		return "Hello World";
+	}
+	
+	public int sum(int a,int b)
+	{
+		return a+b;
+	}
+
+}
